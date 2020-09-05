b55a8057a19093972004b33d813ab3102e9af6b1
Yong Liu(刘勇_BJ) <yongliu@abcft.com>
2020-09-05 10:29:15 +0800

    完成词法分析器

--- Week_02/build-ast/main.js
+++ Week_02/build-ast/main.js
@@ -10,18 +10,36 @@ const dictionary = [
   "-",
 ];
 
-function tokenize(source = "") {
+function* tokenize(source = "") {
   let result = null;
+  let lastIndex = 0;
   while (true) {
+    lastIndex = regexp.lastIndex;
     result = regexp.exec(source);
 
     if (!result) break;
+    if (regexp.lastIndex - lastIndex > result[0].length) {
+      throw new Error("Unrecognized token");
+    }
+
+    let token = {
+      type: null,
+      value: null,
+    };
 
     for (let i = 1; i <= dictionary.length; i++) {
-      if (result[i]) console.log(dictionary[i - 1]);
+      if (result[i]) token.type = dictionary[i - 1];
     }
-    console.log(result);
+
+    token.value = result[0];
+    yield token;
   }
+
+  yield {
+    type: "EOF",
+  };
 }
 
-tokenize("1024 + 10 * 25");
+for (const token of tokenize("1024 + 10 * 25")) {
+  console.log(token);
+}
